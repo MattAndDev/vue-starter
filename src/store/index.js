db33dfba7c5e4761f5794531b77116fa66fec420
Matteo Maronati <me@mattinthe.net>
2018-03-05 12:43:39 +0100

    Setup: SoC for vue-router instance (mutations/actions/state)

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import router from '../router'
-import axios from 'axios'
 import createPersistedState from 'vuex-persistedstate'
+// partials
+import mutations from './mutations'
+import actions from './actions'
+import state from './state'
 
 Vue.use(Vuex)
 const store = new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
+  state: state,
+  mutations: mutations,
+  actions: actions,
   plugins: [
     createPersistedState({
       key: 'my-app'
